02fa89c2e01d620b41a67398f44ee9f188763c51
Lawrence Guo <775569157@qq.com>
2023-12-07 09:31:57 +0800

    调整方形输出的边长；增加ffmpeg的verbose=False形参，减少命令行输出的冗余；删除中间文件

--- python_rotaeno_stabilizer.py
+++ python_rotaeno_stabilizer.py
@@ -5,19 +5,36 @@ import glob
 import os
 import subprocess
 import time
+import math
 
 
-def add_audio_to_video(video_file, audio_source, output_file):
+def add_audio_to_video(video_file, audio_source, output_file, verbose=False):
+    """
+    将音频添加到视频中。
+
+    :param video_file: 输入的视频文件路径。
+    :param audio_source: 输入的音频来源文件路径。
+    :param output_file: 输出视频文件的路径。
+    :param verbose: 是否显示详细的 ffmpeg 输出，默认为 False。
+    :return: None
+    """
+
     command = [
         'ffmpeg',
-        '-i', video_file,  # 输入的视频文件
-        '-i', audio_source,  # 输入的音频来源文件
-        '-c:v', 'copy',
-        '-c:a', 'aac',
+        '-i', video_file,      # 输入的视频文件
+        '-i', audio_source,    # 输入的音频来源文件
+        '-c:v', 'copy',        # 复制视频流
+        '-c:a', 'aac',         # 使用 AAC 编码音频
         '-strict', 'experimental',
-        output_file  # 输出的文件名
+        output_file            # 输出的文件名
     ]
-    subprocess.call(command)
+
+    if not verbose:
+        # 抑制 stdout 和 stderr 输出
+        with open(os.devnull, 'wb') as devnull:
+            subprocess.run(command, stdout=devnull, stderr=devnull)
+    else:
+        subprocess.run(command)
 
 
 def find_mp4_videos():
@@ -34,13 +51,14 @@ def find_mp4_videos():
     return videos
 
 
-def convert_vfr_to_cfr(input_path, output_path, target_framerate=59.97):
+def convert_vfr_to_cfr(input_path, output_path, target_framerate=59.97, verbose=False):
     """
-    VFR转CFR
+    将可变帧率 (VFR) 视频转换为固定帧率 (CFR) 视频。
 
-    :param input_path: 视频路径.
-    :param output_path: 输出路径.
-    :param target_framerate: 目标帧率，默认59.97fps.
+    :param input_path: 输入视频的文件路径。
+    :param output_path: 输出视频的文件路径。
+    :param target_framerate: 目标帧率，默认为 59.97fps。
+    :param verbose: 是否显示详细的 ffmpeg 输出，默认为 False。
     :return: None
     """
 
@@ -48,11 +66,16 @@ def convert_vfr_to_cfr(input_path, output_path, target_framerate=59.97):
         'ffmpeg',
         '-i', input_path,
         '-vf', f'fps={target_framerate}',
-        '-c:a', 'copy',  # Copy audio stream without re-encoding
+        '-c:a', 'copy',  # 复制音频流而不重新编码
         output_path
     ]
 
-    subprocess.run(cmd)
+    if not verbose:
+        # 抑制 stdout 和 stderr 输出
+        with open(os.devnull, 'wb') as devnull:
+            subprocess.run(cmd, stdout=devnull, stderr=devnull)
+    else:
+        subprocess.run(cmd)
 
 
 def get_video_duration(video_path):
@@ -110,17 +133,13 @@ def compute_rotation_v2(top_left_color, top_right_color, bottom_left_color, bott
     # 将RGB值转换为0或1
     def convert_color_to_binary(color):
         array = [1 if c >= 255/2 else 0 for c in color]
-        return array[::-1]
+        return array[::-1] # OpenCV的BGR顺序和RGB相反
 
     # 将四个角的颜色转换为二进制
     binary_top_left = convert_color_to_binary(top_left_color)
     binary_top_right = convert_color_to_binary(top_right_color)
     binary_bottom_left = convert_color_to_binary(bottom_left_color)
     binary_bottom_right = convert_color_to_binary(bottom_right_color)
-    # print("binary_top_left:", binary_top_left)
-    # print("binary_top_right:", binary_top_right)
-    # print("binary_bottom_left:", binary_bottom_left)
-    # print("binary_bottom_right:", binary_bottom_right)
 
     # 将二进制颜色值转换为角度
     color_to_degree = (binary_top_left[0] * 2048 + binary_top_left[1] * 1024 + binary_top_left[2] * 512 +
@@ -132,75 +151,12 @@ def compute_rotation_v2(top_left_color, top_right_color, bottom_left_color, bott
     return -rotation_degree
 
 
-def render(video,type="v2"):
+def render(video,type="v2", square=True): # 渲染方形视频
     '''
 
     :param video: 视频文件名
     :param type: 视频类型，填v1/v2，默认为v2
-    :return: 无返回值，在output文件夹输出渲染完毕的视频
-    '''
-    video_dir = os.path.join(os.getcwd(), 'videos', video)
-    video_file_name = os.path.basename(video)  # 获取不带路径的文件名
-    video_name = os.path.splitext(video_file_name)[0]
-
-    cap = cv2.VideoCapture(video_dir)
-    # fps = round(cap.get(cv2.CAP_PROP_FPS), 2)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    print("fps:", fps)
-
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-
-    output_path = os.path.join(os.getcwd(), 'output', f'{video_name}_stb.mp4')  # 指定输出路径
-    cfr_output_path = os.path.join(os.getcwd(), 'videos', f'{video_name}_cfr.mp4')  # 指定输出路径
-
-    print("正在将视频转换为CFR视频……")
-    convert_vfr_to_cfr(video_dir, cfr_output_path, fps)
-    cap2 = cv2.VideoCapture(cfr_output_path)
-
-    out = cv2.VideoWriter(output_path, fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
-
-    frame_count = int(cap2.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    # 使用tqdm展示进度
-    for _ in tqdm(range(frame_count), desc="Processing video"):
-        ret, frame = cap2.read()
-        if ret:
-            height, width, channels = frame.shape
-
-            # Sample colors
-            O = 5
-            S = 3
-            bottom_left = frame[height - O:height - O + S, O:O + S].mean(axis=(0, 1))
-            top_left = frame[O:O + S, O:O + S].mean(axis=(0, 1))
-            bottom_right = frame[height - O:height - O + S, width - O:width - O + S].mean(axis=(0, 1))
-            top_right = frame[O:O + S, width - O:width - O + S].mean(axis=(0, 1))
-
-            if type == 'v2':
-                angle = compute_rotation_v2(top_left, top_right, bottom_left, bottom_right)
-            else:
-                angle = compute_rotation(top_left, bottom_right, top_right, bottom_left)
-            # print(angle)
-            # Rotate frame
-            M = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
-            rotated_frame = cv2.warpAffine(frame, M, (width, height))
-
-            out.write(rotated_frame)
-            # time.sleep(1 / fps)
-        else:
-            print("Error reading frame")
-
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-    # 用这个方法添加音频，但是目前视频时长不匹配，会导致音画不同步(需要安装ffmpeg)
-    add_audio_to_video(output_path, video_dir, f'output/{video_name}_with_audio.mp4')
-
-def render_square(video,type="v2", square=True): # 渲染方形视频
-    '''
-
-    :param video: 视频文件名
-    :param type: 视频类型，填v1/v2，默认为v2
+    :param square: 是否渲染方形视频
     :return: 无返回值，在output文件夹输出渲染完毕的视频
     '''
     if os.path.isabs(video):
@@ -227,8 +183,10 @@ def render_square(video,type="v2", square=True): # 渲染方形视频
     convert_vfr_to_cfr(video_dir, cfr_output_path, fps)
     cap2 = cv2.VideoCapture(cfr_output_path)
 
+    frame_size = math.ceil(math.sqrt(int(cap.get(3)) ** 2 + int(cap.get(4)) ** 2))
+
     if square: # 方形
-        out = cv2.VideoWriter(output_path, fourcc, fps, (max(int(cap.get(3)), int(cap.get(4))), max(int(cap.get(3)), int(cap.get(4)))))
+        out = cv2.VideoWriter(output_path, fourcc, fps, (frame_size, frame_size))
     else:
         out = cv2.VideoWriter(output_path, fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
 
@@ -255,7 +213,8 @@ def render_square(video,type="v2", square=True): # 渲染方形视频
             # print(angle)
             # Rotate frame
             if square: # 方形渲染
-                max_size = max(height, width)
+                # max_size = max(height, width)
+                max_size = frame_size
                 expanded_frame = np.zeros((max_size, max_size, 3), dtype='uint8')
                 expanded_frame[(max_size - height)//2:(max_size - height)//2 + height, (max_size - width)//2:(max_size - width)//2 + width] = frame
                 M = cv2.getRotationMatrix2D((max_size / 2, max_size / 2), angle, 1)
@@ -273,5 +232,7 @@ def render_square(video,type="v2", square=True): # 渲染方形视频
     out.release()
     cv2.destroyAllWindows()
 
-    # 用这个方法添加音频，但是目前视频时长不匹配，会导致音画不同步(需要安装ffmpeg)
     add_audio_to_video(output_path, video_dir, f'output/{video_name}_with_audio.mp4')
+    os.remove(cfr_output_path)
+    os.remove(output_path)
+    print(f"{video_file_name}稳定完成")
